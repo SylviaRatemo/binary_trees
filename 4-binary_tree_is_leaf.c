f7cd182c929511dab2db26b2bce9405597c9ee3b
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-01 03:41:32 -0700

    Task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right || node->left || node == NULL)
+	if (node == NULL)
 		return (0);
-	return (1);
+	return (!node->right && !node->left);
 }
