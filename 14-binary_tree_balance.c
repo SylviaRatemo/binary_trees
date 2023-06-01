79f3372a6e4d2d8e4e5fe07dcafcf0414626b976
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-01 08:11:23 -0700

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,7 +22,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 /**
  * height - calculate height of BT
  * @tree: pointer to root node
- * return: 0 if NULL, height
+ * Return: 0 if NULL, height
  */
 
 int height(const binary_tree_t *tree)
@@ -30,9 +30,10 @@ int height(const binary_tree_t *tree)
 	if (tree)
 	{
 		int left = 0, right = 0;
-		left = tree->left ? 1 + height(tree->left) : 1;
-                right = tree->right ? 1 + height(tree->right) : 1;
-		return ((left > right) ? left : right);
+
+	left = tree->left ? 1 + height(tree->left) : 1;
+	right = tree->right ? 1 + height(tree->right) : 1;
+	return ((left > right) ? left : right);
 	}
 	return (0);
 }
